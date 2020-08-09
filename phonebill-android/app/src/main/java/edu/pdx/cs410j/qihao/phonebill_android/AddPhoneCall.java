a829eeb338f0fb83edc105e94fe7ddb11147a9f8
Ronnie Song <ronniesong0809@gmail.com>
2020-08-08 23:16:34 -0700

    feat: add date picker

--- phonebill-android/app/src/main/java/edu/pdx/cs410j/qihao/phonebill_android/AddPhoneCall.java
+++ phonebill-android/app/src/main/java/edu/pdx/cs410j/qihao/phonebill_android/AddPhoneCall.java
@@ -1,23 +1,22 @@
 package edu.pdx.cs410j.qihao.phonebill_android;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.ActivityChooserView;
 
+import android.annotation.SuppressLint;
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.io.StringReader;
+import java.util.Calendar;
 
 import edu.pdx.cs410J.ParserException;
 
@@ -36,6 +35,22 @@ public class AddPhoneCall extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_phone_call);
 
+        final EditText startTime = findViewById(R.id.startTime);
+        startTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                datePick(startTime);
+            }
+        });
+
+        final EditText endTime = findViewById(R.id.endTime);
+        endTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                datePick(endTime);
+            }
+        });
+
         Button addCall = findViewById(R.id.add_phone_call_button);
         addCall.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,6 +62,21 @@ public class AddPhoneCall extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    private void datePick(final EditText editText) {
+        final Calendar calendar = Calendar.getInstance();
+        int startYear = calendar.get(Calendar.YEAR);
+        int startMonth = calendar.get(Calendar.MONTH);
+        int startDay = calendar.get(Calendar.DAY_OF_MONTH);
+        DatePickerDialog dialog = new DatePickerDialog(AddPhoneCall.this, new DatePickerDialog.OnDateSetListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                editText.setText(day + "/" + month + "/" + year);
+            }
+        }, startYear, startMonth, startDay);
+        dialog.show();
     }
 
     private void addCall() throws IOException, ParserException {
